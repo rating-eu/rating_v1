0327c1522a2e5215b55582acc71a6c57a76b62f8
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-04-12 10:59:50 +0200

    +Questionnaires module --routing

---
+++ src/main/webapp/app/questionnaires/questionnaires.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { QuestionnairesRoutingModule } from './questionnaires-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    QuestionnairesRoutingModule
+  ],
+  declarations: []
+})
+export class QuestionnairesModule { }
