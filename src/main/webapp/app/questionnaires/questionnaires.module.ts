a06a438c24070353b5f1b2820180826fd2ad210a
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-04-12 11:08:27 +0200

    +Questionnaires ID-TA

--- src/main/webapp/app/questionnaires/questionnaires.module.ts
+++ src/main/webapp/app/questionnaires/questionnaires.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuestionnairesRoutingModule } from './questionnaires-routing.module';
+import { IdTaComponent } from './id-ta/id-ta.component';
 
 @NgModule({
   imports: [
     CommonModule,
     QuestionnairesRoutingModule
   ],
-  declarations: []
+  declarations: [IdTaComponent]
 })
 export class QuestionnairesModule { }
