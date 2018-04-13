953d0c1d5c473864aee6bea64d800bfa2368c27b
Unknown <napolitano.v@gmail.com>
2018-04-13 16:19:32 +0200

    binding identify-assets service with frontend

--- src/main/webapp/app/identify-assets/identify-asset.component.ts
+++ src/main/webapp/app/identify-assets/identify-asset.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Principal } from '../shared';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { IdentifyAssetService } from './identify-asset.service';
+import { AssetMgm } from '../entities/asset-mgm/asset-mgm.model';
 
 @Component({
     selector: 'jhi-identify-asset',
-    templateUrl: './identify-asset.component.html'
+    templateUrl: './identify-asset.component.html',
+    providers: [IdentifyAssetService]
 })
-export class IdentifyAssetComponent implements OnInit {
+export class IdentifyAssetComponent implements OnInit, OnDestroy {
     account: Account;
-
-    constructor(private principal: Principal) {}
+ 	assets: AssetMgm[];
+ 	currentAccount: any;
+    eventSubscriber: Subscription;
+    currentSearch: string;
+     	
+    constructor(
+    	private identifyAssetService: IdentifyAssetService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private activatedRoute: ActivatedRoute,
+        private principal: Principal) {}
 
     ngOnInit() {
-        this.principal.identity().then((account) => {
-            this.account = account;
-        });
+        this.getAllAssets();
+    }
+    
+    getAllAssets(){
+    	this.identifyAssetService.findAll().subscribe(
+    	(res: HttpResponse<AssetMgm[]>) => this.assets = res.body,
+        (res: HttpErrorResponse) => this.onError(res.message));
+    }
+    
+        ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: AssetMgm) {
+        return item.id;
+    }
+    registerChangeInAttackStrategies() {
+        this.eventSubscriber = this.eventManager.subscribe('attackStrategyListModification', (response) => this.getAllAssets());
+    }
+
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 }
