4605756d84b8954996a27a9e22a7fd1ff9eb7630
Unknown <napolitano.v@gmail.com>
2018-04-16 12:20:46 +0200

    format .ts

--- src/main/webapp/app/identify-assets/identify-asset.module.ts
+++ src/main/webapp/app/identify-assets/identify-asset.module.ts
@@ -1,20 +1,20 @@
-  import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-  import {CommonModule} from '@angular/common';
-  import { RouterModule } from '@angular/router';
-  import { IdentifyAssetsRoutingModule } from './identify-assets-routing.module';
-  import { IdentifyAssetComponent } from './id-assets/identify-asset.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IdentifyAssetsRoutingModule } from './identify-assets-routing.module';
+import { IdentifyAssetComponent } from './id-assets/identify-asset.component';
 
-    @NgModule({
-        imports: [
-			CommonModule,
-			IdentifyAssetsRoutingModule
-			
-        ],
-        declarations: [
-            IdentifyAssetComponent,
-        ],
-        exports: [
-        	IdentifyAssetComponent
-        ]
-    })
-    export class IdentifyAssetModule {}
+@NgModule({
+    imports: [
+        CommonModule,
+        IdentifyAssetsRoutingModule
+
+    ],
+    declarations: [
+        IdentifyAssetComponent,
+    ],
+    exports: [
+        IdentifyAssetComponent
+    ]
+})
+export class IdentifyAssetModule { }
