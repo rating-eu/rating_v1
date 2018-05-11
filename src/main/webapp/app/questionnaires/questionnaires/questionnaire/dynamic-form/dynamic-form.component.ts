bb865b7de389fdb97d4df30ee461430b7d33260c
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-05-11 14:11:26 +0200

    +ThreatAgents #YES percentage

--- src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/dynamic-form.component.ts
+++ src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/dynamic-form.component.ts
@@ -3,6 +3,10 @@ import {QuestionControlService} from './services/question-control.service';
 import {FormGroup} from '@angular/forms';
 import {QuestionMgm} from '../../../../entities/question-mgm';
 import {AnswerMgm} from '../../../../entities/answer-mgm';
+import {ThreatAgentMgm} from '../../../../entities/threat-agent-mgm';
+import {Fraction} from '../../../../utils/fraction.class';
+import * as CryptoJS from 'crypto-js';
+import {Couple} from '../../../../utils/couple.class';
 
 @Component({
     selector: 'jhi-dynamic-form',
@@ -11,6 +15,9 @@ import {AnswerMgm} from '../../../../entities/answer-mgm';
     providers: [QuestionControlService]
 })
 export class DynamicFormComponent implements OnInit {
+    private static YES: String = 'YES';
+    private static NO: String = 'NO';
+
     @Input() messageFromParent: String = 'PercheNonFunziona';
     _questionsArray: QuestionMgm[];
     form: FormGroup;
@@ -39,6 +46,8 @@ export class DynamicFormComponent implements OnInit {
     }
 
     ngOnInit() {
+        console.log('Sha256:');
+        console.log(CryptoJS.SHA256('Maurizio').toString());
     }
 
     onSubmit() {
@@ -51,7 +60,7 @@ export class DynamicFormComponent implements OnInit {
 
         console.log('Keys: ' + Object.keys(formData));
 
-        const formDataMap = new Map();
+        const formDataMap = new Map<String, AnswerMgm>();
 
         Object.keys(formData).forEach((key) => {
             formDataMap.set(key, formData[key] as AnswerMgm);
@@ -59,9 +68,47 @@ export class DynamicFormComponent implements OnInit {
 
         console.log('Map: ' + formDataMap);
 
+        const threatAgentsMap: Map<String, Couple<ThreatAgentMgm, Fraction>> = new Map<String, Couple<ThreatAgentMgm, Fraction>>();
+
         formDataMap.forEach((value, key) => {
             console.log('key: ' + key);
             console.log('value: ' + JSON.stringify(value));
+
+            const answer: AnswerMgm = value as AnswerMgm;
+            const threatAgent: ThreatAgentMgm = answer.question.threatAgent;
+            const threatAgentHash = CryptoJS.SHA256(JSON.stringify(threatAgent)).toString();
+
+            if (threatAgentsMap.has(threatAgentHash)) {// a question identifying this threat agent has already been encountered.
+                console.log('Threat agent already processed...');
+
+                // fraction = #YES/#Questions
+                const fraction: Fraction = threatAgentsMap.get(threatAgentHash).value;
+                // increment the number of questions identifying this threat-agent
+                fraction.whole++;
+
+                if (answer.name === DynamicFormComponent.YES) {
+                    console.log('Warning: you answered YES');
+                    fraction.part++;
+                } else if (answer.name === DynamicFormComponent.NO) {
+                    console.log('Good, you answered NO');
+                }
+            } else {// first time
+                console.log('First Time processing this threat agent');
+
+                const fraction = new Fraction(0, 1);
+                threatAgentsMap.set(threatAgentHash, new Couple<ThreatAgentMgm, Fraction>(threatAgent, fraction));
+
+                if (answer.name === DynamicFormComponent.YES) {
+                    console.log('Warning: you answered YES');
+                    fraction.part++;
+                } else if (answer.name === DynamicFormComponent.NO) {
+                    console.log('Good, you answered NO');
+                }
+            }
+        });
+
+        threatAgentsMap.forEach((value, key) => {
+            console.log('ThreatAgent:' + key + ' ==> ' + value.key.name + '\t' + value.value.toPercentage() + '\%');
         });
     }
 }
