cbe67e2dff9fc13652b70ee5a2252fb31f77f2c4
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-05-13 21:59:43 +0200

    +Fix Status Code: 401 Unauthorized (missing JWT)
    The reason behind this exception is that no http-interceptor was provided.
    By looking at the app.module.ts we get the missing piece which seems to fix the authentication problem on API calls from feature-modules services.

--- src/main/webapp/app/identify-threat-agent/identify-threat-agent.module.ts
+++ src/main/webapp/app/identify-threat-agent/identify-threat-agent.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import {HermeneutSharedModule} from '../shared';
 
 import {CommonModule} from '@angular/common';
@@ -6,12 +6,19 @@ import {IdentifyThreatAgentRoutingModule} from './identify-threat-agent-routing.
 import {IdentifyThreatAgentComponent} from './identify-threat-agent.component';
 import {ResultComponent} from './result/result.component';
 import {DatasharingModule} from '../datasharing/datasharing.module';
-import { IdentifyThreatAgentService } from './identify-threat-agent.service';
+import {IdentifyThreatAgentService} from './identify-threat-agent.service';
+import {NotificationInterceptor} from '../blocks/interceptor/notification.interceptor';
+import {AuthInterceptor} from '../blocks/interceptor/auth.interceptor';
+import {LocalStorageService, SessionStorageService} from 'ngx-webstorage';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthExpiredInterceptor} from '../blocks/interceptor/auth-expired.interceptor';
+import {ErrorHandlerInterceptor} from '../blocks/interceptor/errorhandler.interceptor';
+import {JhiEventManager} from 'ng-jhipster';
 
 @NgModule({
     imports: [
-        HermeneutSharedModule,
         CommonModule,
+        HermeneutSharedModule,
         IdentifyThreatAgentRoutingModule,
         DatasharingModule
     ],
@@ -23,7 +30,42 @@ import { IdentifyThreatAgentService } from './identify-threat-agent.service';
         IdentifyThreatAgentComponent,
         ResultComponent
     ],
-    providers: [IdentifyThreatAgentService]
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+            deps: [
+                LocalStorageService,
+                SessionStorageService
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [
+                JhiEventManager
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        },
+        IdentifyThreatAgentService
+    ]
 })
 export class IdentifyThreatAgentModule {
 }
