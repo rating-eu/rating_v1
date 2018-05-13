9773feabae3b84f11589acd5730abd0e9019476f
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-05-13 22:40:00 +0200

    +ThreatAgent-Goals Table (init)

--- src/main/webapp/app/identify-threat-agent/identify-threat-agent.service.ts
+++ src/main/webapp/app/identify-threat-agent/identify-threat-agent.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {SERVER_API_URL} from '../app.constants';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {MotivationMgm} from '../entities/motivation-mgm';
+import {QuestionnaireMgm} from '../entities/questionnaire-mgm';
 
 @Injectable()
 export class IdentifyThreatAgentService {
 
-  constructor() { }
+    motivationsAPI = SERVER_API_URL + 'api/motivations';
 
+    constructor(private http: HttpClient) {
+    }
+
+    findAllMotivations(): Observable<MotivationMgm[]> {
+        return this.http.get<MotivationMgm[]>(this.motivationsAPI);
+    }
 }
--- src/main/webapp/app/identify-threat-agent/result/result.component.ts
+++ src/main/webapp/app/identify-threat-agent/result/result.component.ts
@@ -3,6 +3,8 @@ import {DatasharingService} from '../../datasharing/datasharing.service';
 import {ThreatAgentMgm} from '../../entities/threat-agent-mgm';
 import {Couple} from '../../utils/couple.class';
 import {Fraction} from '../../utils/fraction.class';
+import {IdentifyThreatAgentService} from '../identify-threat-agent.service';
+import {MotivationMgm} from '../../entities/motivation-mgm';
 
 @Component({
     selector: 'jhi-result',
@@ -13,12 +15,18 @@ export class ResultComponent implements OnInit {
 
     threatAgentsMap: Map<String, Couple<ThreatAgentMgm, Fraction>>;
     threatAgentsPercentageArray: Couple<ThreatAgentMgm, Fraction>[];
+    motivations: MotivationMgm[];
 
-    constructor(private dataSharingService: DatasharingService) {
+    constructor(private dataSharingService: DatasharingService, private identifyThreatAgentService: IdentifyThreatAgentService) {
     }
 
     ngOnInit() {
         this.threatAgentsMap = this.dataSharingService.threatAgentsMap;
         this.threatAgentsPercentageArray = Array.from(this.threatAgentsMap.values());
+
+        console.log('Calling to find all motivations...');
+        this.identifyThreatAgentService.findAllMotivations().subscribe((response) => {
+            this.motivations = response as MotivationMgm[];
+        });
     }
 }
