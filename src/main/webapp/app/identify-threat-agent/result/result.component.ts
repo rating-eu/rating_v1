c8a440144abf2ff1ae0d3722a89c966c29696cfb
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-05-16 15:12:42 +0200

    +IdentifiedThreatAgents (SelfAssessment.ThreatAgents --> DB)

--- src/main/webapp/app/identify-threat-agent/result/result.component.ts
+++ src/main/webapp/app/identify-threat-agent/result/result.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DatasharingService} from '../../datasharing/datasharing.service';
 import {ThreatAgentMgm} from '../../entities/threat-agent-mgm';
 import {Couple} from '../../utils/couple.class';
@@ -11,22 +11,26 @@ import {QuestionMgm} from '../../entities/question-mgm';
 import {QuestionnaireMgm} from '../../entities/questionnaire-mgm';
 import {MyAnswerMgm, MyAnswerMgmService} from '../../entities/my-answer-mgm';
 import {Router} from '@angular/router';
-import {SelfAssessmentMgmService} from '../../entities/self-assessment-mgm';
+import {SelfAssessmentMgm, SelfAssessmentMgmService} from '../../entities/self-assessment-mgm';
+import {Subscription} from 'rxjs/Subscription';
+import {QuestionnairesService} from '../../questionnaires/questionnaires.service';
 
 @Component({
     selector: 'jhi-result',
     templateUrl: './result.component.html',
     styles: []
 })
-export class ResultComponent implements OnInit {
-
-    threatAgentsMap: Map<String, Couple<ThreatAgentMgm, Fraction>>;
-    threatAgentsPercentageArray: Couple<ThreatAgentMgm, Fraction>[];
-    motivations: MotivationMgm[];
-    defaultThreatAgents: ThreatAgentMgm[];
-    identifyThreatAgentsFormDataMap: Map<String, AnswerMgm>;
+export class ResultComponent implements OnInit, OnDestroy {
+    private threatAgentsMap: Map<String, Couple<ThreatAgentMgm, Fraction>>;
+    private threatAgentsPercentageArray: Couple<ThreatAgentMgm, Fraction>[];
+    private motivations: MotivationMgm[];
+    private defaultThreatAgents: ThreatAgentMgm[];
+    private identifyThreatAgentsFormDataMap: Map<String, AnswerMgm>;
     private account: Account;
     private user: User;
+    private selfAssessment: SelfAssessmentMgm;
+    private subscriptions: Subscription[] = [];
+    private questionnaire: QuestionnaireMgm;
 
     constructor(private dataSharingService: DatasharingService,
                 private identifyThreatAgentService: IdentifyThreatAgentService,
@@ -34,35 +38,55 @@ export class ResultComponent implements OnInit {
                 private accountService: AccountService,
                 private userService: UserService,
                 private selfAssessmentService: SelfAssessmentMgmService,
-                private router: Router) {
+                private router: Router,
+                private questionnairesService: QuestionnairesService) {
     }
 
     ngOnInit() {
-        this.accountService.get().subscribe((response1) => {
-            this.account = response1.body;
+        this.subscriptions.push(
+            this.accountService.get().subscribe((response1) => {
+                this.account = response1.body;
 
-            this.userService.find(this.account['login']).subscribe((response2) => {
-                this.user = response2.body;
-            });
-        });
+                this.subscriptions.push(
+                    this.userService.find(this.account['login']).subscribe((response2) => {
+                        this.user = response2.body;
+                    })
+                );
+
+                this.selfAssessment = this.selfAssessmentService.getSelfAssessment();
+            })
+        );
 
         this.threatAgentsMap = this.dataSharingService.threatAgentsMap;
         this.threatAgentsPercentageArray = Array.from(this.threatAgentsMap.values());
 
-        this.identifyThreatAgentService.getDefaultThreatAgents().subscribe((response) => {
-            this.defaultThreatAgents = response as ThreatAgentMgm[];
+        this.subscriptions.push(
+            this.identifyThreatAgentService.getDefaultThreatAgents().subscribe((response) => {
+                this.defaultThreatAgents = response as ThreatAgentMgm[];
 
-            this.defaultThreatAgents.forEach((value) => {// Add the default Threat-Agents to the list
-                this.threatAgentsPercentageArray.push(new Couple<ThreatAgentMgm, Fraction>(value, new Fraction(1, 1)));
-            });
-        });
+                this.defaultThreatAgents.forEach((value) => {// Add the default Threat-Agents to the list
+                    this.threatAgentsPercentageArray.push(new Couple<ThreatAgentMgm, Fraction>(value, new Fraction(1, 1)));
+                });
+            })
+        );
 
         console.log('Calling to find all motivations...');
-        this.identifyThreatAgentService.findAllMotivations().subscribe((response) => {
-            this.motivations = response as MotivationMgm[];
-        });
+
+        this.subscriptions.push(
+            this.identifyThreatAgentService.findAllMotivations().subscribe((response) => {
+                this.motivations = response as MotivationMgm[];
+            })
+        );
 
         this.identifyThreatAgentsFormDataMap = this.dataSharingService.identifyThreatAgentsFormDataMap;
+    }
+
+    ngOnDestroy() {
+        if (this.subscriptions) {
+            this.subscriptions.forEach((subscription: Subscription) => {
+                subscription.unsubscribe();
+            });
+        }
     }
 
     hasMotivation(threatAgent: ThreatAgentMgm, motivation: MotivationMgm): boolean {
@@ -91,11 +115,35 @@ export class ResultComponent implements OnInit {
 
             console.log('MyAnser: ' + myAnser);
 
+            // persist the answer of the user
             this.myAnswerService.create(myAnser).subscribe((response) => {
                 const result: MyAnswerMgm = response.body;
                 console.log('MyAnswer response: ' + JSON.stringify(result));
             });
         });
+
+        const threatAgents: ThreatAgentMgm[] = [];
+
+        this.threatAgentsPercentageArray.forEach((threatPercentage) => {
+            const threatAgent: ThreatAgentMgm = threatPercentage.key;
+            console.log('Threat-Agent: ' + JSON.stringify(threatAgent));
+
+            threatAgents.push(threatAgent);
+        });
+
+        const uniqueThreatAgentsSet: Set<ThreatAgentMgm> = new Set<ThreatAgentMgm>(threatAgents.concat(this.selfAssessment.threatagents));
+        const uniqueThreatAgentsArray: ThreatAgentMgm[] = [...uniqueThreatAgentsSet];
+        this.selfAssessment.threatagents = uniqueThreatAgentsArray;
+
+        console.log('AllThreatAgents: ' + JSON.stringify(this.selfAssessment.threatagents));
+
+        // Update the SelfAssessment
+        this.selfAssessmentService.update(this.selfAssessment).subscribe((response) => {
+            const result: SelfAssessmentMgm = response.body;
+            console.log('updated SelfAssessment: ' + JSON.stringify(this.selfAssessment));
+        });
+
+        this.router.navigate(['identify-threat-agent']);
     }
 
     discardIdentfiedThreatAgents() {
