3ad5c950eda691c709db0bef3662cbe0b4151686
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-07-01 17:20:01 +0200

    +JDL Versioning(v1.2.5){Phase Weight} pt. 2

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -21,7 +21,6 @@ import {HermeneutLevelMgmModule} from './level-mgm/level-mgm.module';
 import {HermeneutPhaseMgmModule} from './phase-mgm/phase-mgm.module';
 import {HermeneutCompanyGroupMgmModule} from './company-group-mgm/company-group-mgm.module';
 
-import { HermeneutPhaseMgmModule } from './phase-mgm/phase-mgm.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
