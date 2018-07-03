465f5a5dc41eb6cbd7e582d436c6936267c167aa
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-07-03 12:28:09 +0200

    #42 +Results module (init)

---
+++ src/main/webapp/app/results/results.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResultsRoutingModule } from './results-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ResultsRoutingModule
+  ],
+  declarations: []
+})
+export class ResultsModule { }
