962ba4109b205793eb1758a0302c26d89e7c1d81
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-08-02 16:48:41 +0200

    #28 +Persist ExternalAudit MyAnswers

--- src/main/webapp/app/entities/my-answer-mgm/my-answer-mgm.model.ts
+++ src/main/webapp/app/entities/my-answer-mgm/my-answer-mgm.model.ts
@@ -2,15 +2,16 @@ import { BaseEntity, User } from './../../shared';
 import {AnswerMgm} from '../answer-mgm';
 import {QuestionMgm} from '../question-mgm';
 import {QuestionnaireMgm} from '../questionnaire-mgm';
+import {QuestionnaireStatusMgm} from '../questionnaire-status-mgm';
 
 export class MyAnswerMgm implements BaseEntity {
     constructor(
         public id?: number,
-        public mycheck?: string,
+        public note?: string,
         public answer?: AnswerMgm,
         public question?: QuestionMgm,
         public questionnaire?: QuestionnaireMgm,
-        public questionnaireStatus?: BaseEntity,
+        public questionnaireStatus?: QuestionnaireStatusMgm,
         public user?: User,
     ) {
     }
