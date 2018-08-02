9a0560aa6338ab9112840d470ef77896043664ab
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-08-02 15:06:19 +0200

    JDL 1.3.3(Generated)
    Fix #61

--- src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/dynamic-form.component.ts
+++ src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/dynamic-form.component.ts
@@ -625,7 +625,7 @@ export class DynamicFormComponent implements OnInit, OnDestroy {
                     console.log('Question: ' + JSON.stringify(question));
                     console.log('Questionnaire: ' + JSON.stringify(questionnaire));
 
-                    const myAnser: MyAnswerMgm = new MyAnswerMgm(undefined, 'Checked', answer, question, questionnaire, questionnaireStatus, this.user);
+                    const myAnser: MyAnswerMgm = new MyAnswerMgm(undefined, 'Checked', 0, answer, question, questionnaire, questionnaireStatus, this.user);
 
                     console.log('MyAnser: ' + myAnser);
 
