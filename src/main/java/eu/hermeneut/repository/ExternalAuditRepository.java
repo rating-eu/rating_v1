28f0f51913f4917c4ba5e0105320b79759dfe793
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-09-11 22:48:47 +0200

    +jdl (EA-->User 1-1)

--- src/main/java/eu/hermeneut/repository/ExternalAuditRepository.java
+++ src/main/java/eu/hermeneut/repository/ExternalAuditRepository.java
@@ -4,7 +4,7 @@ import eu.hermeneut.domain.ExternalAudit;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
-import java.util.List;
+
 
 /**
  * Spring Data JPA repository for the ExternalAudit entity.
@@ -12,8 +12,5 @@ import java.util.List;
 @SuppressWarnings("unused")
 @Repository
 public interface ExternalAuditRepository extends JpaRepository<ExternalAudit, Long> {
-
-    @Query("select external_audit from ExternalAudit external_audit where external_audit.user.login = ?#{principal.username}")
-    List<ExternalAudit> findByUserIsCurrentUser();
 
 }
