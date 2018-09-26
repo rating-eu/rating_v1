e760921d8daf837fb346ca2f64743cea6221a304
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-09-26 09:44:19 +0200

    +SelfAssessmentOverview (TABLE)

--- src/main/java/eu/hermeneut/utils/attackstrategy/ThreatAttackFilter.java
+++ src/main/java/eu/hermeneut/utils/attackstrategy/ThreatAttackFilter.java
@@ -3,8 +3,8 @@ package eu.hermeneut.utils.attackstrategy;
 import eu.hermeneut.domain.AttackStrategy;
 import eu.hermeneut.domain.ThreatAgent;
 
-public class AttackStrategyFilter {
-    
+public class ThreatAttackFilter {
+
     public static boolean isAttackPossible(ThreatAgent threatAgent, AttackStrategy attackStrategy) {
         return threatAgent.getSkillLevel().getValue() >= attackStrategy.getSkill().getValue();
     }
