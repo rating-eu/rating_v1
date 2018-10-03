c24bb8010fd71c7c3f79b261dd5310072abcf5e2
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-10-03 12:14:24 +0200

    +GET ImpactLevels by SelfAssessment ID (.TS)

--- src/main/webapp/app/entities/impact-level-mgm/impact-level-mgm.service.ts
+++ src/main/webapp/app/entities/impact-level-mgm/impact-level-mgm.service.ts
@@ -1,51 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { SERVER_API_URL } from '../../app.constants';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {SERVER_API_URL} from '../../app.constants';
 
-import { ImpactLevelMgm } from './impact-level-mgm.model';
-import { createRequestOption } from '../../shared';
+import {ImpactLevelMgm} from './impact-level-mgm.model';
+import {createRequestOption} from '../../shared';
 
 export type EntityResponseType = HttpResponse<ImpactLevelMgm>;
+
+const SELF_ASSESSMENT_ID_PLACEHOLDER = '{selfAssessmentID}';
 
 @Injectable()
 export class ImpactLevelMgmService {
 
-    private resourceUrl =  SERVER_API_URL + 'api/impact-levels';
+    private resourceUrl = SERVER_API_URL + 'api/impact-levels';
+    private resourceUrlBySelfAssessment = SERVER_API_URL + 'api/' + SELF_ASSESSMENT_ID_PLACEHOLDER + '/impact-levels';
     private resourceSearchUrl = SERVER_API_URL + 'api/_search/impact-levels';
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
     create(impactLevel: ImpactLevelMgm): Observable<EntityResponseType> {
         const copy = this.convert(impactLevel);
-        return this.http.post<ImpactLevelMgm>(this.resourceUrl, copy, { observe: 'response' })
+        return this.http.post<ImpactLevelMgm>(this.resourceUrl, copy, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     update(impactLevel: ImpactLevelMgm): Observable<EntityResponseType> {
         const copy = this.convert(impactLevel);
-        return this.http.put<ImpactLevelMgm>(this.resourceUrl, copy, { observe: 'response' })
+        return this.http.put<ImpactLevelMgm>(this.resourceUrl, copy, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     find(id: number): Observable<EntityResponseType> {
-        return this.http.get<ImpactLevelMgm>(`${this.resourceUrl}/${id}`, { observe: 'response'})
+        return this.http.get<ImpactLevelMgm>(`${this.resourceUrl}/${id}`, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     query(req?: any): Observable<HttpResponse<ImpactLevelMgm[]>> {
         const options = createRequestOption(req);
-        return this.http.get<ImpactLevelMgm[]>(this.resourceUrl, { params: options, observe: 'response' })
+        return this.http.get<ImpactLevelMgm[]>(this.resourceUrl, {params: options, observe: 'response'})
+            .map((res: HttpResponse<ImpactLevelMgm[]>) => this.convertArrayResponse(res));
+    }
+
+    findAllBySelfAssessment(selfAssessmentID: number): Observable<HttpResponse<ImpactLevelMgm[]>> {
+        const options = createRequestOption(null);
+        return this.http.get<ImpactLevelMgm[]>(this.resourceUrlBySelfAssessment.replace(SELF_ASSESSMENT_ID_PLACEHOLDER, selfAssessmentID + ''), {
+            params: options,
+            observe: 'response'
+        })
             .map((res: HttpResponse<ImpactLevelMgm[]>) => this.convertArrayResponse(res));
     }
 
     delete(id: number): Observable<HttpResponse<any>> {
-        return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response'});
+        return this.http.delete<any>(`${this.resourceUrl}/${id}`, {observe: 'response'});
     }
 
     search(req?: any): Observable<HttpResponse<ImpactLevelMgm[]>> {
         const options = createRequestOption(req);
-        return this.http.get<ImpactLevelMgm[]>(this.resourceSearchUrl, { params: options, observe: 'response' })
+        return this.http.get<ImpactLevelMgm[]>(this.resourceSearchUrl, {params: options, observe: 'response'})
             .map((res: HttpResponse<ImpactLevelMgm[]>) => this.convertArrayResponse(res));
     }
 
