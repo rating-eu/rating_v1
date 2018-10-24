da83325643d838bb77e667e9cbaa34e5777918f2
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-10-24 12:10:44 +0200

    +Fix #131

--- src/main/java/eu/hermeneut/repository/MyAssetRepository.java
+++ src/main/java/eu/hermeneut/repository/MyAssetRepository.java
@@ -1,6 +1,7 @@
 package eu.hermeneut.repository;
 
 import eu.hermeneut.domain.MyAsset;
+import eu.hermeneut.domain.enumeration.AssetType;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +21,15 @@ public interface MyAssetRepository extends JpaRepository<MyAsset, Long> {
         "SELECT DISTINCT my_asset from MyAsset my_asset LEFT JOIN FETCH my_asset.asset asset " +
             "LEFT JOIN FETCH asset.containers WHERE my_asset.selfAssessment.id = :selfAssessmentID")
     List<MyAsset> findAllBySelfAssessment(@Param("selfAssessmentID") Long selfAssessmentID);
+
+    @Query(
+        "SELECT DISTINCT my_asset from MyAsset my_asset LEFT JOIN FETCH my_asset.asset asset " +
+            "LEFT JOIN FETCH asset.containers LEFT JOIN FETCH asset.assetcategory assetCategory WHERE my_asset" +
+            ".selfAssessment.id = " +
+            ":selfAssessmentID " +
+            "AND assetCategory.type = :assetType")
+    List<MyAsset> findAllBySelfAssessmentAndAssetType(@Param("selfAssessmentID") Long selfAssessmentID, @Param
+        ("assetType") AssetType assetType);
 
     @Query(
         "SELECT DISTINCT my_asset from MyAsset my_asset LEFT JOIN FETCH my_asset.asset asset " +
