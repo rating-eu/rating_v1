d42f40bf2098ba7e590430149a3394d1ab894921
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-11-13 21:24:23 +0100

    cc

--- src/main/java/eu/hermeneut/kafka/producer/KafkaProducer.java
+++ src/main/java/eu/hermeneut/kafka/producer/KafkaProducer.java
@@ -1,16 +1,20 @@
 package eu.hermeneut.kafka.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void send(String topic, String payload) {
         kafkaTemplate.send(topic, payload);
-        System.out.println("Message: " + payload + " sent to topic: " + topic);
+        LOGGER.info("Message: " + payload + " sent to topic: " + topic);
     }
 }
