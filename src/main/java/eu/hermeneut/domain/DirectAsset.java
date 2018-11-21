35a1ccf3879e7418e15f751fd76e4f2dc545f3de
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-11-21 16:43:47 +0100

    +Direct -> {AttackCosts, Indirects} CASCADE

--- src/main/java/eu/hermeneut/domain/DirectAsset.java
+++ src/main/java/eu/hermeneut/domain/DirectAsset.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import javax.persistence.*;
 
 import org.springframework.data.elasticsearch.annotations.Document;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -32,11 +33,11 @@ public class DirectAsset implements Serializable {
     @JoinColumn(unique = true)
     private MyAsset myAsset;
 
-    @OneToMany(mappedBy = "directAsset", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "directAsset", fetch = FetchType.EAGER, cascade = {CascadeType.ALL, CascadeType.PERSIST, CascadeType.MERGE})
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     private Set<AttackCost> costs = new HashSet<>();
 
-    @OneToMany(mappedBy = "directAsset")
+    @OneToMany(mappedBy = "directAsset", cascade = {CascadeType.ALL, CascadeType.PERSIST, CascadeType.MERGE})
     @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     private Set<IndirectAsset> effects = new HashSet<>();
 
