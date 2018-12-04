b7eda0ddc8b9608bb3d30f576a3406d39ddd285a
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-12-04 11:31:43 +0100

    +DashboardStatusAPI: AssetClustering

--- src/main/java/eu/hermeneut/service/impl/DashboardStatusServiceImpl.java
+++ src/main/java/eu/hermeneut/service/impl/DashboardStatusServiceImpl.java
@@ -1,15 +1,42 @@
 package eu.hermeneut.service.impl;
 
-import eu.hermeneut.service.DashboardStatusService;
+import eu.hermeneut.domain.*;
+import eu.hermeneut.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class DashboardStatusServiceImpl implements DashboardStatusService {
+    @Autowired
+    private SelfAssessmentService selfAssessmentService;
+
+    @Autowired
+    private MyAssetService myAssetService;
+
+    @Autowired
+    private DirectAssetService directAssetService;
+
+    @Autowired
+    private IndirectAssetService indirectAssetService;
+
     @Override
     public boolean isAssetClusteringDone(Long selfAssessmentID) {
-        return false;
+        boolean isDone = false;
+        SelfAssessment selfAssessment = this.selfAssessmentService.findOne(selfAssessmentID);
+
+        if (selfAssessment != null) {
+            List<MyAsset> myAssets = this.myAssetService.findAllBySelfAssessment(selfAssessmentID);
+            List<DirectAsset> directAssets = this.directAssetService.findAllBySelfAssessment(selfAssessmentID);
+            List<IndirectAsset> indirectAssets = this.indirectAssetService.findAllBySelfAssessment(selfAssessmentID);
+
+            isDone = !(myAssets.isEmpty() || directAssets.isEmpty() || indirectAssets.isEmpty());
+        }
+
+        return isDone;
     }
 
     @Override
