61821819d6e8581e5787907fed1d0d3f2bc443f6
Maurizio Casciano <mauriziocasciano7@gmail.com>
2018-12-20 21:03:34 +0100

    +GlobalExceptionHandler log

--- src/main/java/eu/hermeneut/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/eu/hermeneut/exceptions/handler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package eu.hermeneut.exceptions.handler;
 import eu.hermeneut.exceptions.IllegalInputException;
 import eu.hermeneut.exceptions.NotFoundException;
 import eu.hermeneut.exceptions.NullInputException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,22 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler({NullInputException.class, IllegalInputException.class})
     public ResponseEntity<Object> nullInputHandler(Exception exception, WebRequest request) {
+        logger.warn(exception.getClass().getName());
+        logger.warn(exception.getMessage());
+
         return new ResponseEntity<>(
             exception.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({NotFoundException.class})
     public ResponseEntity<Object> notFoundHandler(Exception exception, WebRequest request) {
+        logger.warn(exception.getClass().getName());
+        logger.warn(exception.getMessage());
+
         return new ResponseEntity<>(
             exception.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
