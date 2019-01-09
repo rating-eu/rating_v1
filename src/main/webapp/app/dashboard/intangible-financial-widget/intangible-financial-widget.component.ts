dc6be79dece0faec06ac2bf51bda763e901bd6a4
Vincenzo Falanga <ing.vincenzofalanga@gmail.com>
2019-01-09 17:37:31 +0100

    New Widget in dashboard and UI Fix

--- src/main/webapp/app/dashboard/intangible-financial-widget/intangible-financial-widget.component.ts
+++ src/main/webapp/app/dashboard/intangible-financial-widget/intangible-financial-widget.component.ts
@@ -1,4 +1,10 @@
+import * as _ from 'lodash';
+import { MyAssetMgm } from './../../entities/my-asset-mgm/my-asset-mgm.model';
+import { SelfAssessmentMgm } from './../../entities/self-assessment-mgm/self-assessment-mgm.model';
+import { SelfAssessmentMgmService } from './../../entities/self-assessment-mgm/self-assessment-mgm.service';
+import { IdentifyAssetUtilService } from './../../identify-assets/identify-asset.util.service';
 import { Component, OnInit } from '@angular/core';
+import { AssetType } from '../../entities/enumerations/AssetType.enum';
 
 @Component({
   selector: 'jhi-intangible-financial-widget',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['intangible-financial-widget.component.css']
 })
 export class IntangibleFinancialWidgetComponent implements OnInit {
+  private mySelf: SelfAssessmentMgm = {};
 
-  constructor() { }
+  public intangible: MyAssetMgm[];
+  public loading = false;
+  public isCollapsed = true;
+  public assetsPaginator = {
+    id: 'asset_paginator',
+    itemsPerPage: 7,
+    currentPage: 1
+  };
+
+  constructor(
+    private mySelfAssessmentService: SelfAssessmentMgmService,
+    private idaUtilsService: IdentifyAssetUtilService
+  ) { }
 
   ngOnInit() {
+    this.mySelf = this.mySelfAssessmentService.getSelfAssessment();
+    this.idaUtilsService.getMySavedAssets(this.mySelf)
+    .toPromise()
+    .then((mySavedAssets) => {
+      if (mySavedAssets) {
+        if (mySavedAssets.length === 0) {
+          this.loading = false;
+          return;
+        }
+        this.intangible = [];
+        for (const myAsset of mySavedAssets) {
+          if (myAsset.asset.assetcategory.type.toString() === AssetType.INTANGIBLE.toString()) {
+            this.intangible.push(myAsset);
+          }
+        }
+        this.loading = false;
+      } else {
+        this.loading = false;
+      }
+    }).catch(() => {
+      this.loading = false;
+    });
+  }
+
+  onAssetsPageChange(number: number) {
+    this.assetsPaginator.currentPage = number;
   }
 
 }
