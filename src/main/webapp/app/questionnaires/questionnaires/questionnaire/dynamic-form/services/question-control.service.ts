4e932d13e84584a266e5d8e55e3e05f1b0698e2e
Maurizio <mauriziocasciano7@gmail.com>
2019-01-31 16:38:53 +0100

    +ciso EW notes

--- src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/services/question-control.service.ts
+++ src/main/webapp/app/questionnaires/questionnaires/questionnaire/dynamic-form/services/question-control.service.ts
@@ -9,11 +9,25 @@ export class QuestionControlService {
     constructor() {
     }
 
-    toFormGroupCISO(questions: QuestionMgm[]) {
+    toFormGroupCISOIdentifyThreatAgents(questions: QuestionMgm[]) {
         const group: any = {};
 
         questions.forEach((question) => {
             group[question.id] = new FormControl('');
+        });
+
+        const formGroup: FormGroup = new FormGroup(group);
+        formGroup.validator = atLeastOneValidator();
+
+        return formGroup;
+    }
+
+    toFormGroupCISOSelfAssessment(questions: QuestionMgm[]) {
+        const group: any = {};
+
+        questions.forEach((question) => {
+            group[question.id] = new FormControl('');
+            group[question.id + '.ciso.note'] = new FormControl('');
         });
 
         const formGroup: FormGroup = new FormGroup(group);
