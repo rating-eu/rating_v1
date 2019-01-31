efa45f4f55fa07bec2b53cf3a761f41e8ff8527e
Maurizio <mauriziocasciano7@gmail.com>
2019-01-31 13:11:52 +0100

    +Fix dashboard no mySelf selected

--- src/main/webapp/app/dashboard/dashboard-one/dashboard-one.component.ts
+++ src/main/webapp/app/dashboard/dashboard-one/dashboard-one.component.ts
@@ -1,38 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Principal, LoginModalService } from '../../shared';
-import { JhiEventManager } from 'ng-jhipster';
-import { SelfAssessmentMgmService, SelfAssessmentMgm } from '../../entities/self-assessment-mgm';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit} from '@angular/core';
+import {Principal, LoginModalService} from '../../shared';
+import {JhiEventManager} from 'ng-jhipster';
+import {SelfAssessmentMgmService, SelfAssessmentMgm} from '../../entities/self-assessment-mgm';
+import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'jhi-dashboard-one',
-  templateUrl: './dashboard-one.component.html',
-  styleUrls: ['dashboard-one.component.css']
+    selector: 'jhi-dashboard-one',
+    templateUrl: './dashboard-one.component.html',
+    styleUrls: ['dashboard-one.component.css']
 })
 export class DashboardOneComponent implements OnInit {
-  private account: Account;
-  private mySelf: SelfAssessmentMgm = {};
+    private account: Account;
+    private mySelf: SelfAssessmentMgm = {};
 
-  constructor(
-    private principal: Principal,
-    private loginModalService: LoginModalService,
-    private eventManager: JhiEventManager,
-    private mySelfAssessmentService: SelfAssessmentMgmService
-  ) { }
+    constructor(
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private eventManager: JhiEventManager,
+        private mySelfAssessmentService: SelfAssessmentMgmService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.mySelf = this.mySelfAssessmentService.getSelfAssessment();
+    }
 
-  registerAuthenticationSuccess() {
-    this.eventManager.subscribe('authenticationSuccess', (message) => {
-      this.principal.identity().then((account) => {
-        this.account = account;
-      });
-    });
-  }
+    registerAuthenticationSuccess() {
+        this.eventManager.subscribe('authenticationSuccess', (message) => {
+            this.principal.identity().then((account) => {
+                this.account = account;
+            });
+        });
+    }
 
-  isAuthenticated() {
-    return this.principal.isAuthenticated();
-  }
-
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
 }
