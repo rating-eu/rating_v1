17819e9d04119de81aac0c670c45b36d1c97bcb2
Vincenzo Falanga <ing.vincenzofalanga@gmail.com>
2019-02-07 10:55:40 +0100

    Auto stash before merge of "falanga" and "origin/dev"

--- src/main/webapp/app/dashboard/impact-widget/impact-widget.component.ts
+++ src/main/webapp/app/dashboard/impact-widget/impact-widget.component.ts
@@ -1,4 +1,10 @@
+import { ImpactEvaluationStatus } from './../../impact-evaluation/model/impact-evaluation-status.model';
+import { DashboardStepEnum } from './../models/enumeration/dashboard-step.enum';
+import { DashboardService, DashboardStatus } from './../dashboard.service';
+import { ImpactEvaluationService } from './../../impact-evaluation/impact-evaluation.service';
 import { Component, OnInit } from '@angular/core';
+import { SelfAssessmentMgmService, SelfAssessmentMgm } from '../../entities/self-assessment-mgm';
+import { Status } from '../../entities/enumerations/QuestionnaireStatus.enum';
 
 @Component({
   selector: 'jhi-impact-widget',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['impact-widget.component.css']
 })
 export class ImpactWidgetComponent implements OnInit {
+  public loading = false;
+  private mySelf: SelfAssessmentMgm;
+  private status: DashboardStatus;
+  private dashboardStatus = DashboardStepEnum;
 
-  constructor() { }
+  constructor(
+    private impactService: ImpactEvaluationService,
+    private mySelfAssessmentService: SelfAssessmentMgmService,
+    private dashService: DashboardService
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.mySelf = this.mySelfAssessmentService.getSelfAssessment();
+    this.status = this.dashService.getStatus();
+
+    this.dashService.getStatusFromServer(this.mySelf, this.dashboardStatus.ATTACK_RELATED_COSTS).toPromise().then((res) => {
+      this.status.impactEvaluationStatus = Status[res];
+      this.dashService.updateStatus(this.status);
+    });
   }
 
 }
