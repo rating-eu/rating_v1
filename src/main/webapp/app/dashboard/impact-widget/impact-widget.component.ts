0f741aa96ca94dda29a76767bda99bed9a18ace7
Maurizio <mauriziocasciano7@gmail.com>
2019-02-10 13:13:00 +0100

    +Fix wrong status update (override)

--- src/main/webapp/app/dashboard/impact-widget/impact-widget.component.ts
+++ src/main/webapp/app/dashboard/impact-widget/impact-widget.component.ts
@@ -1,37 +1,38 @@
-import { ImpactEvaluationStatus } from './../../impact-evaluation/model/impact-evaluation-status.model';
-import { DashboardStepEnum } from './../models/enumeration/dashboard-step.enum';
-import { DashboardService, DashboardStatus } from './../dashboard.service';
-import { ImpactEvaluationService } from './../../impact-evaluation/impact-evaluation.service';
-import { Component, OnInit } from '@angular/core';
-import { SelfAssessmentMgmService, SelfAssessmentMgm } from '../../entities/self-assessment-mgm';
-import { Status } from '../../entities/enumerations/QuestionnaireStatus.enum';
+import {ImpactEvaluationStatus} from './../../impact-evaluation/model/impact-evaluation-status.model';
+import {DashboardStepEnum} from './../models/enumeration/dashboard-step.enum';
+import {DashboardService, DashboardStatus} from './../dashboard.service';
+import {ImpactEvaluationService} from './../../impact-evaluation/impact-evaluation.service';
+import {Component, OnInit} from '@angular/core';
+import {SelfAssessmentMgmService, SelfAssessmentMgm} from '../../entities/self-assessment-mgm';
+import {Status} from '../../entities/enumerations/QuestionnaireStatus.enum';
 
 @Component({
-  selector: 'jhi-impact-widget',
-  templateUrl: './impact-widget.component.html',
-  styleUrls: ['impact-widget.component.css']
+    selector: 'jhi-impact-widget',
+    templateUrl: './impact-widget.component.html',
+    styleUrls: ['impact-widget.component.css']
 })
 export class ImpactWidgetComponent implements OnInit {
-  public loading = false;
-  private mySelf: SelfAssessmentMgm;
-  private status: DashboardStatus;
-  private dashboardStatus = DashboardStepEnum;
+    public loading = false;
+    private mySelf: SelfAssessmentMgm;
+    private status: DashboardStatus;
+    private dashboardStatus = DashboardStepEnum;
 
-  constructor(
-    private impactService: ImpactEvaluationService,
-    private mySelfAssessmentService: SelfAssessmentMgmService,
-    private dashService: DashboardService
-  ) { }
+    constructor(
+        private impactService: ImpactEvaluationService,
+        private mySelfAssessmentService: SelfAssessmentMgmService,
+        private dashService: DashboardService
+    ) {
+    }
 
-  ngOnInit() {
-    this.loading = true;
-    this.mySelf = this.mySelfAssessmentService.getSelfAssessment();
-    this.status = this.dashService.getStatus();
+    ngOnInit() {
+        this.loading = true;
+        this.mySelf = this.mySelfAssessmentService.getSelfAssessment();
+        this.status = this.dashService.getStatus();
 
-    this.dashService.getStatusFromServer(this.mySelf, this.dashboardStatus.ATTACK_RELATED_COSTS).toPromise().then((res) => {
-      this.status.impactEvaluationStatus = Status[res];
-      this.dashService.updateStepStatus(DashboardStepEnum.ATTACK_RELATED_COSTS, this.status.attackRelatedCostEstimationStatus);
-    });
-  }
+        this.dashService.getStatusFromServer(this.mySelf, this.dashboardStatus.IMPACT_EVALUATION).toPromise().then((res) => {
+            this.status.impactEvaluationStatus = Status[res];
+            this.dashService.updateStepStatus(DashboardStepEnum.ATTACK_RELATED_COSTS, this.status.impactEvaluationStatus);
+        });
+    }
 
 }
