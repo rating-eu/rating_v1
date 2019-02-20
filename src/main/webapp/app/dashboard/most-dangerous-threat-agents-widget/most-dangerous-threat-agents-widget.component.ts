44d1bbb977c4a658b439e31599aac5c60199d90c
Vincenzo Falanga <ing.vincenzofalanga@gmail.com>
2019-02-20 11:27:35 +0100

    +TableOrderBy WIP

--- src/main/webapp/app/dashboard/most-dangerous-threat-agents-widget/most-dangerous-threat-agents-widget.component.ts
+++ src/main/webapp/app/dashboard/most-dangerous-threat-agents-widget/most-dangerous-threat-agents-widget.component.ts
@@ -5,6 +5,14 @@ import { Result } from '../../results/models/result.model';
 import { ResultsService } from '../../results/results.service';
 import { HttpResponse } from '@angular/common/http';
 
+interface OrderBy {
+  threatAgent: boolean;
+  initial: boolean;
+  contextual: boolean;
+  refined: boolean;
+  type: string;
+}
+
 interface MdtaEntity {
   threatAgentID: number;
   threatAgent: string;
@@ -28,6 +36,7 @@ export class MostDangerousThreatAgentsWidgetComponent implements OnInit {
   public loading = false;
   public isCollapsed = true;
   public mdtaEntities: MdtaEntity[];
+  public orderBy: OrderBy;
   public threatAgentsPaginator = {
     id: 'threat_agents_paginator',
     itemsPerPage: 7,
@@ -45,6 +54,13 @@ export class MostDangerousThreatAgentsWidgetComponent implements OnInit {
   ngOnInit() {
     this.loading = true;
     this.mySelf = this.selfAssessmentService.getSelfAssessment();
+    this.orderBy = {
+      threatAgent: false,
+      initial: false,
+      contextual: false,
+      refined: false,
+      type: 'desc'
+    };
     this.resultService.getResult(this.mySelf.id).toPromise().then((res: HttpResponse<Result>) => {
       if (res.body) {
         this.mdtaEntities = [];
@@ -122,5 +138,60 @@ export class MostDangerousThreatAgentsWidgetComponent implements OnInit {
 
   onThreatAgentsPageChange(number: number) {
     this.threatAgentsPaginator.currentPage = number;
+  }
+
+  private resetOrder() {
+    this.orderBy.threatAgent = false;
+    this.orderBy.initial = false;
+    this.orderBy.contextual = false;
+    this.orderBy.refined = false;
+    this.orderBy.type = 'desc';
+  }
+
+  public tableOrderBy(orderColumn: string, desc: boolean) {
+    this.resetOrder();
+    if (desc) {
+      this.orderBy.type = 'desc';
+    } else {
+      this.orderBy.type = 'asc';
+    }
+    switch (orderColumn.toLowerCase()) {
+      case ('threat_agent'): {
+        this.orderBy.threatAgent = true;
+        if (desc) {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['threatAgent'], ['desc']);
+        } else {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['threatAgent'], ['asc']);
+        }
+        break;
+      }
+      case ('initial'): {
+        this.orderBy.initial = true;
+        if (desc) {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['initial'], ['desc']);
+        } else {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['initial'], ['asc']);
+        }
+        break;
+      }
+      case ('contextual'): {
+        this.orderBy.contextual = true;
+        if (desc) {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['contextual'], ['desc']);
+        } else {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['contextual'], ['asc']);
+        }
+        break;
+      }
+      case ('refined'): {
+        this.orderBy.refined = true;
+        if (desc) {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['refined'], ['desc']);
+        } else {
+          this.mdtaEntities = _.orderBy(this.mdtaEntities, ['refined'], ['asc']);
+        }
+        break;
+      }
+    }
   }
 }
