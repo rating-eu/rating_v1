0070ba9849cb47422d6f02750bac13c2c30334f5
Vincenzo Falanga <ing.vincenzofalanga@gmail.com>
2019-02-21 16:11:34 +0100

    BugFix

--- src/main/webapp/app/shared/pipe/replace.pipe.ts
+++ src/main/webapp/app/shared/pipe/replace.pipe.ts
@@ -1,10 +1,13 @@
-import {Pipe, PipeTransform} from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'replace'})
+@Pipe({ name: 'replace' })
 export class ReplacePipe implements PipeTransform {
     transform(value: string, search: string, replace: string): string {
         const regExp: RegExp = new RegExp(search, 'g');
-
-        return value.replace(regExp, replace);
+        if (value) {
+            return value.replace(regExp, replace);
+        } else {
+            return '';
+        }
     }
 }
