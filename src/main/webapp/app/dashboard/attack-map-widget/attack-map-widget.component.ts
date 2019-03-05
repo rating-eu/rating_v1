73e11fe2209948583d67fe409b942d2095f75b2a
Maurizio <mauriziocasciano7@gmail.com>
2019-03-05 23:21:29 +0100

    +Fix AttackMap-Widget catch forkJoin error => loading=false

--- src/main/webapp/app/dashboard/attack-map-widget/attack-map-widget.component.ts
+++ src/main/webapp/app/dashboard/attack-map-widget/attack-map-widget.component.ts
@@ -1,21 +1,21 @@
-import { DashboardStepEnum } from './../models/enumeration/dashboard-step.enum';
-import { DashboardService, DashboardStatus, Status } from './../dashboard.service';
+import {DashboardStepEnum} from './../models/enumeration/dashboard-step.enum';
+import {DashboardService, DashboardStatus, Status} from './../dashboard.service';
 import * as _ from 'lodash';
 
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AugmentedAttackStrategy } from '../../evaluate-weakness/models/augmented-attack-strategy.model';
-import { Observable, Subscription } from 'rxjs';
-import { SelfAssessmentMgm, SelfAssessmentMgmService } from '../../entities/self-assessment-mgm';
-import { ThreatAgentMgm } from '../../entities/threat-agent-mgm';
-import { HttpResponse } from '@angular/common/http';
-import { PhaseMgm, PhaseMgmService } from '../../entities/phase-mgm';
-import { LevelMgm, LevelMgmService } from '../../entities/level-mgm';
-import { AttackStrategyMgm } from '../../entities/attack-strategy-mgm';
-import { LikelihoodStep } from '../../entities/enumerations/LikelihoodStep.enum';
-import { AttackMapService } from '../../evaluate-weakness/attack-map.service';
-import { forkJoin } from 'rxjs/observable/forkJoin';
-import { WeaknessUtils } from '../../evaluate-weakness/utils/weakness-utils';
-import { MatHorizontalStepper } from '@angular/material';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AugmentedAttackStrategy} from '../../evaluate-weakness/models/augmented-attack-strategy.model';
+import {Observable, Subscription} from 'rxjs';
+import {SelfAssessmentMgm, SelfAssessmentMgmService} from '../../entities/self-assessment-mgm';
+import {ThreatAgentMgm} from '../../entities/threat-agent-mgm';
+import {HttpResponse} from '@angular/common/http';
+import {PhaseMgm, PhaseMgmService} from '../../entities/phase-mgm';
+import {LevelMgm, LevelMgmService} from '../../entities/level-mgm';
+import {AttackStrategyMgm} from '../../entities/attack-strategy-mgm';
+import {LikelihoodStep} from '../../entities/enumerations/LikelihoodStep.enum';
+import {AttackMapService} from '../../evaluate-weakness/attack-map.service';
+import {forkJoin} from 'rxjs/observable/forkJoin';
+import {WeaknessUtils} from '../../evaluate-weakness/utils/weakness-utils';
+import {MatHorizontalStepper} from '@angular/material';
 
 @Component({
     selector: 'jhi-attack-map-widget',
@@ -93,68 +93,68 @@ export class AttackMapWidgetComponent implements OnInit, OnDestroy {
         this.attackMatrix$ = this.attackMapService.getAttackCKC7Matrix(this.selfAssessment.id);
 
         const join$: Observable<[HttpResponse<PhaseMgm[]>, HttpResponse<LevelMgm[]>, Map<Number, Map<Number, AugmentedAttackStrategy>>]> =
-            forkJoin(this.ckc7Phases$, this.attackLevels$, this.attackMatrix$).catch((err: any, caught: any) => {
-                    this.loading = false;
-                    return new Observable<[null, null, null]>();
-                });
+            forkJoin(this.ckc7Phases$, this.attackLevels$, this.attackMatrix$);
 
         join$.subscribe((response: [HttpResponse<PhaseMgm[]>, HttpResponse<LevelMgm[]>, Map<Number, Map<Number, AugmentedAttackStrategy>>]) => {
-            this.ckc7Phases = response[0].body;
-            // Remove id 7 phase
-            const index = _.findIndex(this.ckc7Phases, (phase) => phase.id === 7);
+                this.ckc7Phases = response[0].body;
+                // Remove id 7 phase
+                const index = _.findIndex(this.ckc7Phases, (phase) => phase.id === 7);
 
-            if (index !== -1) {
-                this.ckc7Phases.splice(index, 1);
-            }
+                if (index !== -1) {
+                    this.ckc7Phases.splice(index, 1);
+                }
 
-            this.attackLevels = response[1].body;
-            this.attacksCKC7Matrix = response[2];
+                this.attackLevels = response[1].body;
+                this.attacksCKC7Matrix = response[2];
 
-            this.augmentedAttackStrategiesMap = new Map<number, AugmentedAttackStrategy>();
+                this.augmentedAttackStrategiesMap = new Map<number, AugmentedAttackStrategy>();
 
-            // Make same ID AttackStrategies point to the same AugmentedAttackStrategy object
-            for (const levelID of Object.keys(this.attacksCKC7Matrix)) {
-                for (const phaseID of Object.keys(this.attacksCKC7Matrix[levelID])) {
-                    const augmentedAttackStrategies: Array<AugmentedAttackStrategy> = this.attacksCKC7Matrix[Number(levelID)][Number(phaseID)];
-                    const augmentedAttackStrategiesByReference: Array<AugmentedAttackStrategy> = [];
+                // Make same ID AttackStrategies point to the same AugmentedAttackStrategy object
+                for (const levelID of Object.keys(this.attacksCKC7Matrix)) {
+                    for (const phaseID of Object.keys(this.attacksCKC7Matrix[levelID])) {
+                        const augmentedAttackStrategies: Array<AugmentedAttackStrategy> = this.attacksCKC7Matrix[Number(levelID)][Number(phaseID)];
+                        const augmentedAttackStrategiesByReference: Array<AugmentedAttackStrategy> = [];
 
-                    for (let augmentedAttackStrategy of augmentedAttackStrategies) {
-                        if (!this.augmentedAttackStrategiesMap.has(augmentedAttackStrategy.id)) {
-                            this.augmentedAttackStrategiesMap.set(augmentedAttackStrategy.id, augmentedAttackStrategy);
+                        for (let augmentedAttackStrategy of augmentedAttackStrategies) {
+                            if (!this.augmentedAttackStrategiesMap.has(augmentedAttackStrategy.id)) {
+                                this.augmentedAttackStrategiesMap.set(augmentedAttackStrategy.id, augmentedAttackStrategy);
 
-                            // Each time that we encounter an AttackStrategy we put it in the array by Reference
-                            augmentedAttackStrategiesByReference.push(augmentedAttackStrategy);
-                        } else {
-                            augmentedAttackStrategy = this.augmentedAttackStrategiesMap.get(augmentedAttackStrategy.id);
-                            // Each time that we encounter an AttackStrategy we put it in the array by Reference
-                            augmentedAttackStrategiesByReference.push(augmentedAttackStrategy);
+                                // Each time that we encounter an AttackStrategy we put it in the array by Reference
+                                augmentedAttackStrategiesByReference.push(augmentedAttackStrategy);
+                            } else {
+                                augmentedAttackStrategy = this.augmentedAttackStrategiesMap.get(augmentedAttackStrategy.id);
+                                // Each time that we encounter an AttackStrategy we put it in the array by Reference
+                                augmentedAttackStrategiesByReference.push(augmentedAttackStrategy);
+                            }
                         }
+                        // Finally we replace the initial AttackStrategies with those by REFERENCE
+                        // (to allow one-time update for all the occurrences of the same AttackStrategy in different Levels or Phases)
+                        this.attacksCKC7Matrix[Number(levelID)][Number(phaseID)] = augmentedAttackStrategiesByReference;
                     }
-                    // Finally we replace the initial AttackStrategies with those by REFERENCE
-                    // (to allow one-time update for all the occurrences of the same AttackStrategy in different Levels or Phases)
-                    this.attacksCKC7Matrix[Number(levelID)][Number(phaseID)] = augmentedAttackStrategiesByReference;
                 }
-            }
-            // Check which steps (INITIAL, CONTEXTUAL, REFINED) are available.
-            const allAugmentedAttackStrategies: AugmentedAttackStrategy[] = Array.from(this.augmentedAttackStrategiesMap.values());
+                // Check which steps (INITIAL, CONTEXTUAL, REFINED) are available.
+                const allAugmentedAttackStrategies: AugmentedAttackStrategy[] = Array.from(this.augmentedAttackStrategiesMap.values());
 
-            if (allAugmentedAttackStrategies && allAugmentedAttackStrategies.length > 0) {
-                const augmentedAttackStrategy: AugmentedAttackStrategy = allAugmentedAttackStrategies[0];
-                if (augmentedAttackStrategy.initialLikelihood > 0) {
-                    this.likelihoodStepEnabled.set(LikelihoodStep.INITIAL_LIKELIHOOD, true);
+                if (allAugmentedAttackStrategies && allAugmentedAttackStrategies.length > 0) {
+                    const augmentedAttackStrategy: AugmentedAttackStrategy = allAugmentedAttackStrategies[0];
+                    if (augmentedAttackStrategy.initialLikelihood > 0) {
+                        this.likelihoodStepEnabled.set(LikelihoodStep.INITIAL_LIKELIHOOD, true);
+                    }
+                    if (augmentedAttackStrategy.contextualLikelihood > 0) {
+                        this.likelihoodStepEnabled.set(LikelihoodStep.CONTEXTUAL_LIKELIHOOD, true);
+                    }
+                    if (augmentedAttackStrategy.refinedLikelihood > 0) {
+                        this.likelihoodStepEnabled.set(LikelihoodStep.REFINED_LIKELIHOOD, true);
+                    }
                 }
-                if (augmentedAttackStrategy.contextualLikelihood > 0) {
-                    this.likelihoodStepEnabled.set(LikelihoodStep.CONTEXTUAL_LIKELIHOOD, true);
-                }
-                if (augmentedAttackStrategy.refinedLikelihood > 0) {
-                    this.likelihoodStepEnabled.set(LikelihoodStep.REFINED_LIKELIHOOD, true);
-                }
-            }
 
-            // Set the SelectedThreatAgent (NULL) to make sure all the AttackStrategies are disabled at start.
-            this.threatAgentChanged(this.selectedThreatAgent);
-            this.loading = false;
-        });
+                // Set the SelectedThreatAgent (NULL) to make sure all the AttackStrategies are disabled at start.
+                this.threatAgentChanged(this.selectedThreatAgent);
+                this.loading = false;
+            },
+            (error: any) => {
+                this.loading = false;
+            });
     }
 
     ngOnDestroy(): void {
