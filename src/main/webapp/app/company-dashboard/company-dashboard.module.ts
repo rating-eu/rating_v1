3207afff74f99bafad7edfeb7b84860ee7b9ee20
Maurizio <mauriziocasciano7@gmail.com>
2019-05-08 18:14:12 +0200

    +VulnerabilityRadarWidget (init)

--- src/main/webapp/app/company-dashboard/company-dashboard.module.ts
+++ src/main/webapp/app/company-dashboard/company-dashboard.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CompanyDashboardRoutingModule } from './company-dashboard-routing.module';
 import { EntryPointComponent } from './entry-point/entry-point.component';
+import { VulnerabilityRadarWidgetComponent } from './vulnerability-radar-widget/vulnerability-radar-widget.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CompanyDashboardRoutingModule
   ],
-  declarations: [EntryPointComponent]
+  declarations: [EntryPointComponent, VulnerabilityRadarWidgetComponent]
 })
 export class CompanyDashboardModule { }
