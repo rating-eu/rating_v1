62f643b04d282935682272d79fab43a83f2c82ad
Maurizio <mauriziocasciano7@gmail.com>
2019-05-15 14:50:42 +0200

    +SelfAssessment.ImpactMode (Java)

--- src/main/java/eu/hermeneut/domain/SelfAssessment.java
+++ src/main/java/eu/hermeneut/domain/SelfAssessment.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 HERMENEUT Consortium
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
 
 package eu.hermeneut.domain;
 
+import eu.hermeneut.domain.enumeration.ImpactMode;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
@@ -76,6 +77,11 @@ public class SelfAssessment implements Serializable {
 
     @ManyToOne
     private ExternalAudit externalAudit;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "impact_mode", nullable = false)
+    private ImpactMode impactMode = ImpactMode.QUANTITATIVE;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
     public Long getId() {
@@ -185,6 +191,14 @@ public class SelfAssessment implements Serializable {
 
     public void setCompanyGroups(Set<CompanyGroup> companyGroups) {
         this.companyGroups = companyGroups;
+    }
+
+    public ImpactMode getImpactMode() {
+        return impactMode;
+    }
+
+    public void setImpactMode(ImpactMode impactMode) {
+        this.impactMode = impactMode;
     }
 
     // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
