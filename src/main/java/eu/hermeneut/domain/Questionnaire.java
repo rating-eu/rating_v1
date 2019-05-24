81280e6c531b62d9db5a27b8904b985e235badf0
Maurizio <mauriziocasciano7@gmail.com>
2019-05-24 15:11:09 +0200

    +Questionnaire uniqueConstraint (purpose, company_type){Java}

--- src/main/java/eu/hermeneut/domain/Questionnaire.java
+++ src/main/java/eu/hermeneut/domain/Questionnaire.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 HERMENEUT Consortium
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,9 +38,11 @@ import eu.hermeneut.domain.enumeration.QuestionnairePurpose;
  * A Questionnaire.
  */
 @Entity
-@Table(name = "questionnaire")
+@Table(
+    name = "questionnaire",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"purpose", "company_type"})
+)
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-
 public class Questionnaire implements Serializable {
 
     private static final long serialVersionUID = 1L;
