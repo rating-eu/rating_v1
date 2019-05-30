3d3982c668cab9177104fffd7d51b98bae2d84c2
Maurizio <mauriziocasciano7@gmail.com>
2019-05-30 18:53:16 +0200

    +QuestionnaireStatus.created & modified (Java)

--- src/main/java/eu/hermeneut/web/rest/QuestionnaireStatusResource.java
+++ src/main/java/eu/hermeneut/web/rest/QuestionnaireStatusResource.java
@@ -113,6 +113,15 @@ public class QuestionnaireStatusResource {
         ZonedDateTime now = ZonedDateTime.now();
         questionnaireStatus.setModified(now);
 
+        if (questionnaireStatus.getRefinement() != null) {
+            if (questionnaireStatus.getRefinement().getCreated() == null) {
+                questionnaireStatus.getRefinement().setCreated(now);
+            }
+
+            //Always update the modified date
+            questionnaireStatus.getRefinement().setModified(now);
+        }
+
         QuestionnaireStatus result = questionnaireStatusService.save(questionnaireStatus);
         return ResponseEntity.ok()
             .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, questionnaireStatus.getId().toString()))
