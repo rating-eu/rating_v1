e3bb98f9741e0f9c4c8dc7456d3e7c0634e675d7
Maurizio <mauriziocasciano7@gmail.com>
2019-06-06 12:58:07 +0200

    +Filter MyAssetRisks by indirectlyVulnerable || directlyVulnerable

--- src/main/java/eu/hermeneut/service/impl/wp4/MyAssetRiskServiceImpl.java
+++ src/main/java/eu/hermeneut/service/impl/wp4/MyAssetRiskServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 HERMENEUT Consortium
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -88,6 +88,11 @@ public class MyAssetRiskServiceImpl implements MyAssetRiskService, MaxValues {
         if (myAssets == null || myAssets.isEmpty()) {
             throw new NotFoundException("MyAssets NOT Found!!!");
         }
+
+        // Keep only the assets that are directly or indirectly vulnerable
+        myAssets = myAssets.stream().parallel()
+            .filter(myAsset -> myAsset.getAsset().getDirectlyVulnerable() || myAsset.getAsset().getIndirectlyVulnerable())
+            .collect(Collectors.toList());
 
         Set<AssetRisk> assetRisks = this.assetRiskService.getAssetRisks(selfAssessmentID);
 
