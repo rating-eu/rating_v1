17edb67e16aaf07b7ff3151cc1c0b29547a29a3c
Maurizio <mauriziocasciano7@gmail.com>
2019-06-06 18:19:10 +0200

    +Added FortAwesome 5+

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -282,6 +282,7 @@ export class SidebarComponent implements OnInit, AfterViewInit {
             },
             {
                 label: 'Taxonomies',
+                icon: 'fas fa-atom',
                 items: [
                     {
                         label: 'Assets',
