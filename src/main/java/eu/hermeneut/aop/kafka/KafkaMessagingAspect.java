8182c336179b4069d555f596f06d042fbffcf0e0
Maurizio <mauriziocasciano7@gmail.com>
2019-06-12 17:33:55 +0200

    +Send VulnerabilityProfile to Kafka

--- src/main/java/eu/hermeneut/aop/kafka/KafkaMessagingAspect.java
+++ src/main/java/eu/hermeneut/aop/kafka/KafkaMessagingAspect.java
@@ -17,6 +17,7 @@
 
 package eu.hermeneut.aop.kafka;
 
+import eu.hermeneut.domain.CompanyProfile;
 import eu.hermeneut.domain.MyAsset;
 import eu.hermeneut.domain.QuestionnaireStatus;
 import eu.hermeneut.domain.SelfAssessment;
@@ -66,29 +67,63 @@ public class KafkaMessagingAspect {
         Object[] args = joinPoint.getArgs();
 
         if (args != null && args.length > 0) {
-            //The first parameter must be the ID of the SelfAssessment
-            if (args[0] instanceof Long) {
-                selfAssessment = this.selfAssessmentService.findOne((Long) args[0]);
-            } else if (args[0] instanceof MyAsset) {
+            if (args[0] instanceof MyAsset) {
                 MyAsset myAsset = (MyAsset) args[0];
 
                 if (myAsset.getImpact() != null && myAsset.getImpact() > 0) {
                     selfAssessment = myAsset.getSelfAssessment();
                 }
-            } else if (args[0] instanceof QuestionnaireStatus) {
-                QuestionnaireStatus questionnaireStatus = (QuestionnaireStatus) args[0];
+            }
+        }
 
-                if (questionnaireStatus.getStatus().equals(Status.PENDING) || questionnaireStatus.getStatus().equals(Status.FULL)) {
-                    //TODO Fix-Me: CompanyProfile instead of SelfAssessment
-                    //selfAssessment = questionnaireStatus.getSelfAssessment();
+        if (selfAssessment != null) {
+            try {
+                this.messageSenderService.sendRiskProfile(selfAssessment.getId());
+            } catch (Exception e) {
+                logger.warn(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Pointcut for methods annotated with KafkaVulnerabilityProfileHook.
+     */
+    @Pointcut("@annotation(eu.hermeneut.aop.annotation.KafkaVulnerabilityProfileHook)")
+    public void kafkaVulnerabilityProfileHook() {
+    }
+
+    /**
+     * Cross-cutting method to send the updated VulnerabilityProfile to Kafka.
+     *
+     * @param joinPoint
+     */
+    @AfterReturning("kafkaVulnerabilityProfileHook()")
+    public void sendVulnerabilityProfileToKafka(JoinPoint joinPoint) {
+        CompanyProfile companyProfile = null;
+
+        Object[] args = joinPoint.getArgs();
+
+        if (args != null) {
+            switch (args.length) {
+                case 0: {
+                    // Do nothing
+                    break;
+                }
+                case 1: {
+                    if (args[0] instanceof QuestionnaireStatus) {
+                        QuestionnaireStatus questionnaireStatus = (QuestionnaireStatus) args[0];
+
+                        if (questionnaireStatus != null) {
+                            companyProfile = questionnaireStatus.getCompanyProfile();
+                        }
+                    }
                 }
             }
         }
 
-        //TODO Fix-Me
-        if (selfAssessment != null) {
+        if (companyProfile != null) {
             try {
-                this.messageSenderService.sendRiskProfile(selfAssessment.getId());
+                this.messageSenderService.sendVulnerabilityProfile(companyProfile.getId());
             } catch (Exception e) {
                 logger.warn(e.getMessage());
             }
