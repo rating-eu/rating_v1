f2e4cde9483fe82a49bcea55c3f468babfbfadb1
Maurizio <mauriziocasciano7@gmail.com>
2019-06-14 11:41:36 +0200

    +(Risk)VulnerabilityProfileController (Secured for CISO & ADMIN)

--- src/main/java/eu/hermeneut/web/rest/compact/RiskProfileController.java
+++ src/main/java/eu/hermeneut/web/rest/compact/RiskProfileController.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 HERMENEUT Consortium
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,13 @@ package eu.hermeneut.web.rest.compact;
 import com.codahale.metrics.annotation.Timed;
 import eu.hermeneut.domain.compact.input.RiskProfile;
 import eu.hermeneut.exceptions.NotFoundException;
+import eu.hermeneut.security.AuthoritiesConstants;
 import eu.hermeneut.service.compact.RiskProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,27 +40,11 @@ public class RiskProfileController {
     @Autowired
     private RiskProfileService riskProfileService;
 
-    //@Autowired
-    //private MessageSenderService messageSenderService;
-
     @GetMapping("{selfAssessmentID}/risk-profile")
+    @PreAuthorize("@selfAssessmentGuardian.isCISO(#selfAssessmentID) || hasRole('ROLE_ADMIN')")
+    @Secured({AuthoritiesConstants.CISO, AuthoritiesConstants.ADMIN})
     @Timed
     public RiskProfile getRiskProfile(@PathVariable("selfAssessmentID") Long selfAssessmentID) throws NotFoundException {
         return this.riskProfileService.getRiskProfile(selfAssessmentID);
     }
-
-    /*@GetMapping("{selfAssessmentID}/risk-profile/kafka")
-    @Timed
-    public void sendRiskProfile(@PathVariable("selfAssessmentID") Long selfAssessmentID) {
-        RiskProfile riskProfile = null;
-
-        try {
-            riskProfile = this.riskProfileService.getRiskProfile(selfAssessmentID);
-            this.messageSenderService.sendRiskProfile(selfAssessmentID);
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        } catch (NullInputException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
