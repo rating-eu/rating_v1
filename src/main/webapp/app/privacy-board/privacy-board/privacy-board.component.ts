6389391c87c932bdeeb400d357dda97712095077
Maurizio <mauriziocasciano7@gmail.com>
2019-10-02 15:06:13 +0200

    +Added PrivacyBoard redirect if NO DataOperation
    This usually happens on page reloading, 'cause the properties previously stored in the DataSharing service get lost.

--- src/main/webapp/app/privacy-board/privacy-board/privacy-board.component.ts
+++ src/main/webapp/app/privacy-board/privacy-board/privacy-board.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {DataSharingService} from "../../data-sharing/data-sharing.service";
-import {DataOperationMgm} from "../../entities/data-operation-mgm";
-import {Subscription} from "rxjs";
+import {DataSharingService} from '../../data-sharing/data-sharing.service';
+import {DataOperationMgm} from '../../entities/data-operation-mgm';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'jhi-privacy-board',
@@ -13,13 +14,18 @@ export class PrivacyBoardComponent implements OnInit, OnDestroy {
     public dataOperation: DataOperationMgm;
     private subscriptions: Subscription[];
 
-    constructor(private dataSharingService: DataSharingService) {
+    constructor(private router: Router,
+                private dataSharingService: DataSharingService) {
     }
 
     ngOnInit() {
         this.subscriptions = [];
 
         this.dataOperation = this.dataSharingService.dataOperation;
+
+        if (!this.dataOperation) {
+            this.router.navigate(['/privacy-risk-assessment/operations']);
+        }
 
         this.subscriptions.push(
             this.dataSharingService.dataOperation$.subscribe(
