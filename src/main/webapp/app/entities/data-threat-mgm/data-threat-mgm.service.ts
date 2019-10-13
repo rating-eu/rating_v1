bb9b178c6cf170919bbdc256559872ba994a1145
Maurizio <mauriziocasciano7@gmail.com>
2019-10-13 21:39:15 +0200

    +PrivacyBoard OverallDataThreat detail widget

--- src/main/webapp/app/entities/data-threat-mgm/data-threat-mgm.service.ts
+++ src/main/webapp/app/entities/data-threat-mgm/data-threat-mgm.service.ts
@@ -1,45 +1,56 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { SERVER_API_URL } from '../../app.constants';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {SERVER_API_URL} from '../../app.constants';
 
-import { DataThreatMgm } from './data-threat-mgm.model';
-import { createRequestOption } from '../../shared';
+import {DataThreatMgm} from './data-threat-mgm.model';
+import {createRequestOption} from '../../shared';
 
 export type EntityResponseType = HttpResponse<DataThreatMgm>;
+
+const OPERATION_ID = '{operationID}';
 
 @Injectable()
 export class DataThreatMgmService {
 
-    private resourceUrl =  SERVER_API_URL + 'api/data-threats';
+    private resourceUrl = SERVER_API_URL + 'api/data-threats';
+    private resourceByDataOperationUrl = SERVER_API_URL + 'api/data-threats/operation/' + OPERATION_ID;
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
     create(dataThreat: DataThreatMgm): Observable<EntityResponseType> {
         const copy = this.convert(dataThreat);
-        return this.http.post<DataThreatMgm>(this.resourceUrl, copy, { observe: 'response' })
+        return this.http.post<DataThreatMgm>(this.resourceUrl, copy, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     update(dataThreat: DataThreatMgm): Observable<EntityResponseType> {
         const copy = this.convert(dataThreat);
-        return this.http.put<DataThreatMgm>(this.resourceUrl, copy, { observe: 'response' })
+        return this.http.put<DataThreatMgm>(this.resourceUrl, copy, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     find(id: number): Observable<EntityResponseType> {
-        return this.http.get<DataThreatMgm>(`${this.resourceUrl}/${id}`, { observe: 'response'})
+        return this.http.get<DataThreatMgm>(`${this.resourceUrl}/${id}`, {observe: 'response'})
             .map((res: EntityResponseType) => this.convertResponse(res));
     }
 
     query(req?: any): Observable<HttpResponse<DataThreatMgm[]>> {
         const options = createRequestOption(req);
-        return this.http.get<DataThreatMgm[]>(this.resourceUrl, { params: options, observe: 'response' })
+        return this.http.get<DataThreatMgm[]>(this.resourceUrl, {params: options, observe: 'response'})
+            .map((res: HttpResponse<DataThreatMgm[]>) => this.convertArrayResponse(res));
+    }
+
+    getAllByDataOperation(operationID: number): Observable<HttpResponse<DataThreatMgm[]>> {
+        const options = createRequestOption();
+        return this.http.get<DataThreatMgm[]>(this.resourceByDataOperationUrl.replace(OPERATION_ID, String(operationID)),
+            {params: options, observe: 'response'})
             .map((res: HttpResponse<DataThreatMgm[]>) => this.convertArrayResponse(res));
     }
 
     delete(id: number): Observable<HttpResponse<any>> {
-        return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response'});
+        return this.http.delete<any>(`${this.resourceUrl}/${id}`, {observe: 'response'});
     }
 
     private convertResponse(res: EntityResponseType): EntityResponseType {
--- src/main/webapp/app/privacy-board/overall-data-threat-widget/overall-data-threat-widget.component.ts
+++ src/main/webapp/app/privacy-board/overall-data-threat-widget/overall-data-threat-widget.component.ts
@@ -1,4 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnInit, ViewRef} from '@angular/core';
+import {DataOperationMgm} from "../../entities/data-operation-mgm";
+import {Observable} from "rxjs";
+import {HttpHeaders, HttpResponse} from "@angular/common/http";
+import {OverallSecurityImpactMgm} from "../../entities/overall-security-impact-mgm";
+import {SecurityImpactMgm} from "../../entities/security-impact-mgm";
+import {forkJoin} from "rxjs/observable/forkJoin";
+import {ThreatArea} from "../../entities/enumerations/gdpr/ThreatArea.enum";
+import {DataThreatMgm, DataThreatMgmService} from "../../entities/data-threat-mgm";
+import {OverallDataThreatMgm, OverallDataThreatMgmService} from "../../entities/overall-data-threat-mgm";
+import {DataThreatLikelihood} from "../../entities/enumerations/gdpr/DataThreatLikelihood.enum";
 
 @Component({
     selector: 'jhi-overall-data-threat-widget',
@@ -8,11 +18,70 @@ import {Component, OnInit} from '@angular/core';
 export class OverallDataThreatWidgetComponent implements OnInit {
 
     public loading = false;
-    
-    constructor() {
+
+    // Properties
+    private _dataOperation: DataOperationMgm;
+
+    public threatAreas: ThreatArea[];
+    public dataThreats: DataThreatMgm[];
+    public dataThreatsMap: Map<ThreatArea, DataThreatMgm>;
+    public overallDataThreat: OverallDataThreatMgm;
+    public threatLikelihoodEnum = DataThreatLikelihood;
+
+    constructor(private dataThreatService: DataThreatMgmService,
+                private overallDataThreatService: OverallDataThreatMgmService,
+                private changeDetector: ChangeDetectorRef) {
     }
 
     ngOnInit() {
+        this.threatAreas = Object.keys(ThreatArea).map((key) => ThreatArea[key]);
+
     }
 
+    @Input()
+    set dataOperation(dataOperation: DataOperationMgm) {
+        this._dataOperation = dataOperation;
+
+        if (this._dataOperation && this._dataOperation.id) {
+            const overallThreat$: Observable<HttpResponse<OverallDataThreatMgm>> = this.overallDataThreatService.getByDataOperation(this._dataOperation.id);
+            let individualThreats$: Observable<HttpResponse<DataThreatMgm[]>> = null;
+
+            if (this._dataOperation.impacts && this._dataOperation.impacts.length) {
+                const response: HttpResponse<DataThreatMgm[]> = new HttpResponse({
+                    body: this._dataOperation.threats,
+                    headers: new HttpHeaders(),
+                    status: 200
+                });
+
+                individualThreats$ = Observable.of(response);
+            } else {
+                individualThreats$ = this.dataThreatService.getAllByDataOperation(this._dataOperation.id);
+            }
+
+            const join$: Observable<[HttpResponse<DataThreatMgm[]>, HttpResponse<OverallDataThreatMgm>]> = forkJoin(individualThreats$, overallThreat$);
+
+            join$.toPromise().then(
+                (response: [HttpResponse<SecurityImpactMgm[]>, HttpResponse<OverallSecurityImpactMgm>]) => {
+                    if (response) {
+                        this.dataThreats = response[0].body;
+                        this.overallDataThreat = response[1].body;
+
+                        this.dataThreatsMap = new Map();
+
+                        this.dataThreats.forEach((threat: DataThreatMgm) => {
+                            this.dataThreatsMap.set(threat.threatArea, threat);
+                        });
+
+                        if (this.changeDetector && !(this.changeDetector as ViewRef).destroyed) {
+                            this.changeDetector.detectChanges();
+                        }
+                    }
+                }
+            );
+        }
+    }
+
+    get dataOperation(): DataOperationMgm {
+        return this._dataOperation;
+    }
 }
