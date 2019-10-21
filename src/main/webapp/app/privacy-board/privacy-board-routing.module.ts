603f7b66adf5fb3ea0c9f5511129fd8bdbce8785
Maurizio <mauriziocasciano7@gmail.com>
2019-10-21 12:29:09 +0200

    +GDPR Page titles

--- src/main/webapp/app/privacy-board/privacy-board-routing.module.ts
+++ src/main/webapp/app/privacy-board/privacy-board-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {PrivacyBoardComponent} from './privacy-board/privacy-board.component';
-import {DataOperationContextComponent} from '../privacy-risk-assessment/data-operation-context/data-operation-context.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: PrivacyBoardComponent
+        component: PrivacyBoardComponent,
+        data: {
+            pageTitle: 'gdpr.privacy-board.page.title'
+        }
     }
 ];
 
