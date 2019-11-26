c5651afc8d176e5cdc9fcdef3e6d216e4f548b20
Maurizio <mauriziocasciano7@gmail.com>
2019-11-26 17:42:13 +0100

    +AOP UpdateQuestionRelevances (Java)

--- src/main/java/eu/hermeneut/aop/annotation/UpdateQuestionRelevancesHook.java
+++ src/main/java/eu/hermeneut/aop/annotation/UpdateQuestionRelevancesHook.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 HERMENEUT Consortium
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,23 +15,14 @@
  *
  */
 
-package eu.hermeneut.domain.enumeration;
+package eu.hermeneut.aop.annotation;
 
-public enum AnswerLikelihood {
-    LOW(5),
-    LOW_MEDIUM(4),
-    MEDIUM(3),
-    MEDIUM_HIGH(2),
-    HIGH(1),
-    NOT_APPLICABLE(0);
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-    private final int value;
-
-    AnswerLikelihood(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD) //can use on methods only.
+public @interface UpdateQuestionRelevancesHook {
 }
