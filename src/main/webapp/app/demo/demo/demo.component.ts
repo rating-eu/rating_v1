57bfdfb792db1117b2e283dc19ace002de822092
Maurizio <mauriziocasciano7@gmail.com>
2020-01-31 16:51:16 +0100

    +Demo Module (Lazy Loading)

---
+++ src/main/webapp/app/demo/demo/demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-demo',
+  templateUrl: './demo.component.html',
+  styles: []
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
